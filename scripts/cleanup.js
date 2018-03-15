a90e2901eb5826b5b6c6c06841c115026d08e9c8
Valdi <vmurin@valinor.me>
2018-03-15 21:53:36 +0100

    Remove windows limitation

--- scripts/cleanup.js
+++ scripts/cleanup.js
@@ -1,8 +1,3 @@
-if (process.platform === 'win32') {
-  console.error('Must be run on a Unix OS');
-  process.exit(1);
-}
-
 var execSync = require('child_process').execSync;
 var fs = require('fs');
 
